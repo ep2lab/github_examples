91516b4f84133b3472ebfe7eae01511d221e12b5
Pedro Jimenez <pejimene@ing.uc3m.es>
2024-11-25 12:06:24 +0100

    Update math_library.py

--- math_library.py
+++ math_library.py
@@ -3,6 +3,15 @@ def scalar_product(vector1, vector2):
         raise ValueError("Vectors must be of the same length")
     return sum(x * y for x, y in zip(vector1, vector2))
 
+def vector_product(vector1, vector2):
+    if len(vector1) != 3 or len(vector2) != 3:
+        raise ValueError("Both vectors must be of length 3")
+    return [
+        vector1[1] * vector2[2] - vector1[2] * vector2[1],
+        vector1[2] * vector2[0] - vector1[0] * vector2[2],
+        vector1[0] * vector2[1] - vector1[1] * vector2[0]
+    ]
+
 def matrix_product(matrix1, matrix2):
     if len(matrix1[0]) != len(matrix2):
         raise ValueError("Number of columns in the first matrix must be equal to the number of rows in the second matrix")
@@ -15,12 +24,3 @@ def matrix_product(matrix1, matrix2):
                 result[i][j] += matrix1[i][k] * matrix2[k][j]
     
     return result
-
-def vector_product(vector1, vector2):
-    if len(vector1) != 3 or len(vector2) != 3:
-        raise ValueError("Both vectors must be of length 3")
-    return [
-        vector1[1] * vector2[2] - vector1[2] * vector2[1],
-        vector1[2] * vector2[0] - vector1[0] * vector2[2],
-        vector1[0] * vector2[1] - vector1[1] * vector2[0]
-    ]
