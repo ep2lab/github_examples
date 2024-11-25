d6e14ce5db0ae943d22df60c8df838bb642c38f2
jieweiz <jzhou@pa.uc3m.es>
2024-11-25 11:54:11 +0100

    Matrix product.

--- math_library.py
+++ math_library.py
@@ -2,3 +2,15 @@ def scalar_product(vector1, vector2):
     if len(vector1) != len(vector2):
         raise ValueError("Vectors must be of the same length")
     return sum(x * y for x, y in zip(vector1, vector2))
+def matrix_product(matrix1, matrix2):
+    if len(matrix1[0]) != len(matrix2):
+        raise ValueError("Number of columns in the first matrix must be equal to the number of rows in the second matrix")
+    
+    result = [[0 for _ in range(len(matrix2[0]))] for _ in range(len(matrix1))]
+    
+    for i in range(len(matrix1)):
+        for j in range(len(matrix2[0])):
+            for k in range(len(matrix2)):
+                result[i][j] += matrix1[i][k] * matrix2[k][j]
+    
+    return result
